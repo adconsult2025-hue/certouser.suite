fdfef537c4d83f670a108f4255e19dc98d4797bb
adconsult2025-hue <adconsult2025@gmail.com>
2025-09-24 00:58:58 +0200

    Update cer-list.js

--- netlify/functions/cer-list.js
+++ netlify/functions/cer-list.js
@@ -1,10 +1,16 @@
+// netlify/functions/cer-list.js
 const { withClient, ensureSchema, success, failure } = require('./_db.js');
 
 exports.handler = async () => {
   try {
     return await withClient(async (client) => {
       await ensureSchema(client);
-      const res = await client.query('SELECT id, name, cabina, quota_shared, split_prod, split_prosumer, split_cer_to_user, trader, created_at FROM cer ORDER BY created_at DESC LIMIT 200');
+      const res = await client.query(`
+        SELECT id, name, cabina, quota_shared, split_prod, split_prosumer, split_cer_to_user, trader, created_at
+        FROM cer
+        ORDER BY created_at DESC
+        LIMIT 200
+      `);
       return success({ items: res.rows });
     });
   } catch (e) {
